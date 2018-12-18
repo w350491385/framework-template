3b487027ecfe0ad8ce748eead634938520f6c3eb
huangdb002 <huangdb002@qianhai.com.cn>
2018-12-18 17:14:31 +0800

    提交代码

---
+++ src/test/java/com/bn/Test.java
@@ -0,0 +1,37 @@
+package com.bn;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+
+import java.io.File;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Test {
+    public static void main(String[] args) throws Exception {
+        //创建一个freemarker.template.Configuration实例，它是存储 FreeMarker 应用级设置的核心部分
+        //指定版本号
+       String path = Test.class.getClassLoader().getResource("").getPath();
+       System.out.println("path is " + path);
+        Configuration cfg=new Configuration(Configuration.VERSION_2_3_22);
+        //设置模板目录
+        cfg.setDirectoryForTemplateLoading(new File("src/main/resources/templates"));
+        //设置默认编码格式
+        cfg.setDefaultEncoding("UTF-8");
+        //数据
+        Map<String, Object> product = new HashMap<>();
+        product.put("name", "Huwei P8");
+        product.put("price", "3985.7");
+        product.put("users", new String[]{"Tom","Jack","Rose"});
+        //从设置的目录中获得模板
+        Template temp = cfg.getTemplate("product.ftl");
+        //合并模板和数据模型
+        Writer out = new OutputStreamWriter(System.out);
+        temp.process(product, out);
+        //关闭
+        out.flush();
+        out.close();
+    }
+}
